088dd9674ae76c35a48e7e54bdeffcc31f0d4eb5
RABI46 <59933472+RABI46@users.noreply.github.com>
2025-05-06 23:53:00 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import Dashboard from "../src/components/Dashboard";
+import CigaretteTracker from "../src/components/CigaretteTracker";
+
+export default function HomePage() {
+  return (
+    <>
+      <Dashboard />
+      <CigaretteTracker />
+    </>
+  );
+}

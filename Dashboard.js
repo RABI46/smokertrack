e5db4229865370b3240a487f388a7734ef02d7db
RABI46 <59933472+RABI46@users.noreply.github.com>
2025-05-06 23:51:41 +0200

    Create Dashboard.js

---
+++ Dashboard.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Dashboard() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>🔥 SmokeTracker - Suivi des cigarettes 🚀</Text>
+    </View>
+  );
+}
